8cab864b2d565b1a99cd01e223943d10776a73eb
Leonard Tye <leonardtye1992@gmail.com>
2018-05-25 00:25:03 +0800

    client index

--- client/index.js
+++ client/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {
-	Provider
-} from 'react-redux';
-import reduxThunk from 'redux-thunk';
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import {
-	setCurrentUser
-} from './modules/User/UserActions'
-import axios from 'axios'
+	render
+} from 'react-dom';
+
 import App from './App';
-import {
-	store
-} from './store'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-
-if (localStorage.token) {
-	const token = localStorage.token
-	axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-	store.dispatch(setCurrentUser(token))
-} else {
-	delete axios.defaults.headers.common['Authorization'];
-}
-
-const muiTheme = getMuiTheme({
-	tabs: {
-		backgroundColor: 'white',
-		textColor: 'black',
-		selectedTextColor: 'black'
-	}
-})
-
-ReactDOM.render(
-	<MuiThemeProvider muiTheme={muiTheme} >
-    <Provider store={store}>
-        <App />
-    </Provider>
-</MuiThemeProvider>,
-	document.getElementById("root")
-)
+render(
+	<App />, document.getElementById('root')
+);
